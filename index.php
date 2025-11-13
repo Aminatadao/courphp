5ec23e04eacf18613c6497145501a6bca8df48f7
amina <aminata.dao12@outlook.fr>
2025-11-13 15:40:06 +0100

    initialisation du projet

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Portefolio</title>
+</head>
+<body>
+
+<header>
+
+ 
+  <nav>
+    <div class="ent"> 
+    <img src="img/photo.png" alt="photo identité" class="photo">
+    <h1>AMINATA DAO<br>DEVELOPPEUSE WEB</h1> 
+     </div>
+    <ul> 
+     <li> <a href=""> À propos</a></li>
+     <li><a href="">Expérience</a></li>
+     <li><a href="">Formation</a></li>
+     <li><a href="">Compétences</a></li>
+     <li><a href="">Projets</a></li>
+     <li><a href="">Contact</a></li>
+    </ul>
+ </nav>
+
+</header>
+
+ <body>
+
+
+                            <!-- SECTION A PROPOS -->
+    <section id="Apropos">
+    <p>
+      Étudiante en Bachelor 3 Développement Web et Applicatif à l'IESIG, je suis passionnée par la création d'expériences numériques modernes.
+      Je maîtrise le HTML et le CSS, et je poursuis actuellement mon apprentissage de JavaScript et PHP.
+      Curieuse, motivée et volontaire, je m'investis pleinement dans chaque projet afin d'allier esthétique, performance et fonctionnalité.
+      Mon objectif est d'évoluer dans une équipe dynamique où je pourrai contribuer activement tout en renforçant mon expertise technique et créative.
+    </p>
+  
+
+    </section>
+
+                        <!--  SECTION EXPERIENCE -->
+
+      <section id="experience">
+
+
+
+
+
+      </section>
+
+                            <!-- section competence -->
+
+<section id="competence">
+
+
+
+    </section>
+
+
+
+
+                               <!-- section projets -->
+
+  <section  id="projets">
+
+
+
+
+
+             </section>
+
+             <!-- section contact -->
+
+           <section id="contact">
+
+
+           </section>   
+  
+
+
+
+<!-- footer -->
+<footer>
+    <div class="footer-content">
+        <h3>Mon Portfolio</h3>
+        <p>Étudiante en Bachelor 3 Développement Web et Applicatif à l’IESIG.  
+        Passionnée par le web et motivée à créer des solutions modernes et efficaces.</p>
+
+        <ul class="socials">
+            
+            <li><a href="#" title="GitHub"><img src="assets/img/github.png" alt="GitHub"></a></li>
+            <li><a href="#" title="Email"><img src="assets/img/mail.png" alt="Mail"></a></li>
+        </ul>
+    </div>
+
+    <div class="footer-bottom">
+        <p>&copy; 2025 - Mon Portfolio | Réalisé avec passion par AMINATA DAO</p>
+    </div>
+</footer>
+
+
+
+
+
+
+
+</body>
+</html>
